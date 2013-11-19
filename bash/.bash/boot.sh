55f1bb57173413002d50f46139947143008548fd
Lukas Kuklinek <ilja.kuklic@centrum.cz>
2013-11-19 18:10:30 +0000

    bash support boot script

---
+++ bash/.bash/boot.sh
@@ -0,0 +1,46 @@
+# Bash bootstrap functions
+
+function source-dir-ext {
+	local EXT="$1"
+	shift
+	for DIR in "$@"; do
+		[ -d "$DIR" ] && for SRC in "$DIR/"*"$EXT"; do
+			source "$SRC"
+		done
+	done
+}
+
+function source-dir { source-dir-ext ".sh" "$@"; }
+
+function env-set {
+	local NAME="$1"
+	shift
+	export "$NAME=$*"
+}
+
+function env-add {
+	local NAME="$1"
+	shift
+	eval "export '$NAME=\$$NAME:$*'"
+}
+
+function env-prepend {
+	local NAME="$1"
+	shift
+	eval "export '$NAME=$*:\$$NAME'"
+}
+
+function env-alias {
+	local NAME="$1"
+	shift
+	alias "$NAME=$*"
+}
+
+function env-include {
+	source "$*"
+}
+
+function env-boot {
+	source-dir-ext .env .{local/share,config}/shenv
+}
+
