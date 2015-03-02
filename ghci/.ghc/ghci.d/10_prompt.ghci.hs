2151293c442e77c33c3fccd62ea92efb144e6d0a
Lukas Kuklinek <ilja.kuklic@centrum.cz>
2015-03-02 17:01:47 +0000

    GHCi confing in one file

--- ghci/.ghc/ghci.d/10_prompt.ghci.hs
+++
@@ -1,3 +0,0 @@
--- prompt
--- TODO load colour from the environment
-:set prompt "\ESC[0;38;5;166m\STXλ>\ESC[m\STX "
--- ghci/.ghc/ghci.d/50_extensions.ghci.hs
+++
@@ -1,7 +0,0 @@
--- type extensions
-:seti -XNoMonomorphismRestriction -XGADTs -XDeriveFunctor
--- syntax extensions
-:seti -XViewPatterns -XArrows -XTypeOperators
--- language extensons
-:seti -XMonadComprehensions -XTemplateHaskell
-
