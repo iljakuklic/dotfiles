32caf8dec1a0059bd05f94613d02a923fe7268a4
LukasK <ilja.kuklic@centrum.cz>
2020-01-13 00:50:55 +0000

    bash: function to move up directory hierarchy

--- bash/.config/bashrc.d/40_navigation.sh
+++ bash/.config/bashrc.d/40_navigation.sh
@@ -24,3 +24,42 @@ function mkcd {
   done
   mkdir "${MKDIRARGS[@]}" && cd "${CDARGS[@]}"
 }
+
+# Move up the directory hierarchy
+function cdup {
+  case "$1" in
+    -h|--help)
+      echo "cdup: Move up the directory hierarchy"
+      echo
+      echo 'Usage:'
+      echo '  cdup       - Move to the parent directory'
+      echo '  cdup N     - Jump N directories up'
+      echo '  cdup DIR   - Jump up to particular directory (prefix is ok)'
+      ;;
+    -*)
+      echo "Unknown switch '$1'" >&2
+      return 1
+      ;;
+    '')
+      cd ..
+      ;;
+    0)
+      ;;
+    [1-9]|1[0-9])
+      cd $(printf "../%.0s" `seq $1`)
+      ;;
+    *)
+      local PAT="${1#/}"
+      local DIR="$PWD"
+      while [ "$DIR" != '' ]; do
+        if [[ "${DIR##*/}/" == "$PAT"* ]]; then
+          cd "$DIR"
+          return 0
+        fi
+        DIR="${DIR%/*}"
+      done
+      echo "Pattern '$PAT' not found in current path" >&2
+      return 1
+      ;;
+  esac
+}
