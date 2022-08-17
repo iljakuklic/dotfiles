42cb37dec74c86d9c6bc8ec706f44ac2b94720a1
LukasK <ilja.kuklic@centrum.cz>
2022-08-17 11:32:07 +0100

    nvim: Config cleanup, remove treesitter for now

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,15 +10,12 @@ require('packer').startup(function()
     use 'wbthomason/packer.nvim'
     use 'editorconfig/editorconfig-vim'
     use 'tomasiser/vim-code-dark'
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use { 'SmiteshP/nvim-gps', requires = 'nvim-treesitter/nvim-treesitter' }
-    use { 'nvim-treesitter/nvim-treesitter-textobjects', requires = 'nvim-treesitter/nvim-treesitter' }
-    use { 'nvim-treesitter/playground', requires = 'nvim-treesitter/nvim-treesitter', cmd = 'TSPlaygroundToggle' }
     use { 'hoob3rt/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
     use { 'neovim/nvim-lspconfig' }
     use 'hrsh7th/cmp-nvim-lsp'
-    --use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/nvim-cmp'
+    use 'simrat39/rust-tools.nvim'
 end)
 
 -- Editing
@@ -54,53 +51,8 @@ vim.cmd('au ColorScheme * hi Normal ctermbg=none')
 vim.cmd('au ColorScheme * hi EndOfBuffer ctermbg=none')
 vim.cmd('au ColorScheme * hi NonText ctermbg=none')
 
--- Treesitter
-vim.cmd('packadd nvim-treesitter')
-require('nvim-treesitter.configs').setup {
-    ensure_installed = 'maintained',
-    highlight = {enable = true},
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "[n",
-            node_incremental = "[n",
-            node_decremental = "]n",
-            scope_incremental = "[N",
-        }
-    },
-    playground = {
-        enable = true,
-        persist_queries = true,
-    },
-    textobjects = {
-        select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-            },
-        },
-    },
-}
-
--- Status line
-nvim_gps = require('nvim-gps')
-nvim_gps.setup({ separator = '  ' })
-require('lualine').setup {
-    sections = {
-        lualine_c = {
-            { 'filename', path = 1 },
-            { nvim_gps.get_location, condition = nvim_gps.is_available },
-        },
-        lualine_x = {},
-    },
-}
-
 -- Completion
-local cmp = require'cmp'
+local cmp = require('cmp')
 
 cmp.setup({
     snippet = {
@@ -108,8 +60,6 @@ cmp.setup({
         expand = function(args)
             vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
             -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-            -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
         end,
     },
     mapping = {
@@ -122,6 +72,24 @@ cmp.setup({
             c = cmp.mapping.close(),
         }),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            --elseif luasnip.expand_or_jumpable() then
+            --    luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            --elseif luasnip.jumpable(-1) then
+            --    luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
     },
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
@@ -133,15 +101,15 @@ cmp.setup({
 -- Language servers
 local nvim_lsp = require('lspconfig')
 
-local on_attach = function(client, bufnr)
+local on_lsp_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
     -- Enable completion triggered by <c-x><c-o>
     buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    -- Mappings.
-    local opts = { noremap=true, silent=true }
+    -- Mapping opts
+    local opts = { noremap = true, silent = true }
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
@@ -153,29 +121,27 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
     buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
     buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    buf_set_keymap('n', '<leader>n', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
     buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
     buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
     buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local lsp_servers = { 'rust_analyzer' }
-local lsp_capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-for _, lsp in ipairs(lsp_servers) do
-    nvim_lsp[lsp].setup {
-        capabilities = lsp_capabilities,
-        on_attach = on_attach,
-        flags = {
-            debounce_text_changes = 150,
-        }
-    }
-end
+-- LSP configuration options
+local lsp_config = {
+    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    on_attach = on_lsp_attach,
+    --standalone = true,
+    flags = { debounce_text_changes = 150 },
+}
+
+--nvim_lsp.rust_analyzer.setup(lsp_config)
+require('rust-tools').setup({
+    tools = {},
+    server = lsp_config,
+})
 
 -- Key bindings
 keymap = vim.api.nvim_set_keymap
