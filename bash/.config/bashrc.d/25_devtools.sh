ce312609ef10eb17e83f8b4389a940cfbffb7650
iljakuklic <ilja.kuklic@centrum.cz>
2025-10-10 19:16:27 +0100

    bash: load gh completions lazily

--- bash/.config/bashrc.d/25_devtools.sh
+++ bash/.config/bashrc.d/25_devtools.sh
@@ -1,2 +1,13 @@
 # Check github command, set up completions if present
-iscmd gh --eval completion -s bash
+#iscmd gh --eval completion -s bash
+if iscmd gh; then
+  __COMPLETION_DONE_GH=false
+  function __lazy_start_gh {
+    if ! $__COMPLETION_DONE_GH; then
+      source <(gh completion -s bash)
+      __COMPLETION_DONE_GH=true
+      __start_gh "$@"
+    fi
+  }
+  complete -o default -F __lazy_start_gh gh
+fi
